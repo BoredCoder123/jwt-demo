e1da29bbf8ad2b9376f4db8e62d47df97baec4c4
Ankit Kathait <ankitkathait28@gmail.com>
2022-01-19 20:03:14 +0530

    Returning jwt

---
+++ jwt-demo/src/main/java/com/example/jwtdemo/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.example.jwtdemo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+    private String username;
+    private String password;
+}
