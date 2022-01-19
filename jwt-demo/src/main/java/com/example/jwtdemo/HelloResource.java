b9cf2c60838a7f63066abb7e04aa64ba63061665
Ankit Kathait <ankitkathait28@gmail.com>
2022-01-19 19:37:52 +0530

    basic security

--- jwt-demo/src/main/java/com/example/jwtdemo/HelloResource.java
+++ jwt-demo/src/main/java/com/example/jwtdemo/HelloResource.java
@@ -1,11 +1,11 @@
 package com.example.jwtdemo;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloResource {
-    @RequestMapping({"/hello"})
+    @GetMapping("/hello")
     public String hello(){
         return "Hello world";
     }
